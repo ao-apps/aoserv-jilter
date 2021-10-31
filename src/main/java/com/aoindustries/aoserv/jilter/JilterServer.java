b36e3e3212c1d948ee351ee972e5ee0f8775290d
AO Industries, Inc <support@aoindustries.com>
2021-10-30 23:57:24 -0500

    Changed links to https for https://www.gnu.org/licenses/

--- src/main/java/com/aoindustries/aoserv/jilter/JilterServer.java
+++ src/main/java/com/aoindustries/aoserv/jilter/JilterServer.java
@@ -18,7 +18,7 @@
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with aoserv-jilter.  If not, see <http://www.gnu.org/licenses/>.
+ * along with aoserv-jilter.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.aoindustries.aoserv.jilter;
 
