108088d10a8be883d07b689866885422fc68a566
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:00:23 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoindustries/aoserv/jilter/JilterServer.java
+++ src/main/java/com/aoindustries/aoserv/jilter/JilterServer.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with aoserv-jilter.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.aoserv.jilter;
 
 import com.aoindustries.aoserv.jilter.config.JilterConfiguration;
--- src/main/java/com/aoindustries/aoserv/jilter/Notice.java
+++ src/main/java/com/aoindustries/aoserv/jilter/Notice.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-jilter - Mail filter for the AOServ Platform.
- * Copyright (C) 2007-2011, 2021  AO Industries, Inc.
+ * Copyright (C) 2007-2011, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with aoserv-jilter.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoindustries.aoserv.jilter;
 
 /**
