c8fb0fc49255753a07aa043cd216849ad18718c4
AO Industries, Inc <support@aoindustries.com>
2024-10-12 01:02:08 -0500

    checkstyle-10.18.2 compatibility

--- src/main/java/com/aoindustries/aoserv/jilter/AoservJilterHandler.java
+++ src/main/java/com/aoindustries/aoserv/jilter/AoservJilterHandler.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-jilter - Mail filter for the AOServ Platform.
- * Copyright (C) 2007-2013, 2020, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2007-2013, 2020, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -188,6 +188,9 @@ public class AoservJilterHandler implements JilterHandler {
 
   //private String authType;
 
+  /**
+   * Creates a new {@link AoservJilterHandler}.
+   */
   public AoservJilterHandler() throws IOException {
     init();
   }
